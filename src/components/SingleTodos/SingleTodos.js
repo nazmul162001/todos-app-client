a8fde6d7ccfa4f281be6716f43bfc525d585a11d
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-19 03:10:10 +0600

    fixed bugs

--- src/components/SingleTodos/SingleTodos.js
+++ src/components/SingleTodos/SingleTodos.js
@@ -1,23 +1,32 @@
+import axios from 'axios';
 import React from 'react';
 import { GiCheckMark } from 'react-icons/gi';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 
 const SingleTodos = ({ todo, handeDelete }) => {
-  const { todoTask, todoDescription, _id } = todo;
+  const { todoTask, todoDescription, _id , complete} = todo;
 
+  const handleComplited = async () => {
+    await axios.put(
+      `http://localhost:5000/api/user/${_id}`,
+      { complete: complete ? false : true }
+    );
+  };
 
   return (
     <div>
       <table class="table w-full my-2">
         <tbody>
           <tr className="border-2 w-full">
-              {/* <td className="py-4 px-1"> {todoTask} </td>
+            {/* <td className="py-4 px-1"> {todoTask} </td>
               <td className="py-4 px-1"> {todoDescription} </td> */}
-              <td>
-                <span className="py-4 px-1 text-xl text-gray-500 font-bold">{todoTask}</span>
-                <br />
-                <span className="py-4 px-1">{todoDescription}</span>
-              </td>
+            <td>
+              <span className="py-4 px-1 text-xl text-gray-500 font-bold">
+                {todoTask}
+              </span>
+              <br />
+              <span className="py-4 px-1">{todoDescription}</span>
+            </td>
             <div className="todos-action flex justify-end items-center">
               <td className="py-4 px-1">
                 <button class="btn btn-xs btn-success text-sm">
@@ -25,7 +34,10 @@ const SingleTodos = ({ todo, handeDelete }) => {
                 </button>
               </td>
               <td className="py-4 px-1">
-                <button onClick={()=> handeDelete(_id)} class="btn btn-xs btn-error  text-sm">
+                <button
+                  onClick={() => handeDelete(_id)}
+                  class="btn btn-xs btn-error  text-sm"
+                >
                   Delete <RiDeleteBin6Line className="ml-2 text-xl" />{' '}
                 </button>
               </td>
